47348050e05a100ea4d45686721f0d65c6491eac
Jason Newell <newell.jasonb@gmail.com>
2015-02-03 16:20:06 -0600

    Views and view controllers set up

---
+++ LocalNotificationsTutorial/TodoSchedulingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TodoSchedulingViewController.swift
+//  LocalNotificationsTutorial
+//
+//  Created by Jason Newell on 1/30/15.
+//
+//
+
+import UIKit
+
+class TodoSchedulingViewController: UIViewController {
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var deadlinePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func savePressed(sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
